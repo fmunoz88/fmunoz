0283a86f530f9286d0a55503e7b272b748457d0e
Fabian <fmunoz88@outlook.com>
2014-07-28 13:57:47 -0500

    archivos

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prueba Diseño</title>
+ 	<meta charset="utf-8">
+ 	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<nav id="navigation">
+		<div id="nav-inner">
+			<div class="logo">LOGO</div>
+			<div class="nav-menu">
+				<ul>
+					<li>
+						<a href="">menú 1</a>
+					</li>
+					<li>
+						<a href="">menú 2</a>
+					</li>
+					<li>
+						<a href="">menú 3</a>
+					</li>
+					<li>
+						<a href="">menú 4</a>
+					</li> 
+					<li>
+						<a href="">menú 5</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<section id="home" class="">
+		<div id="home-inner">
+		</div>
+	</section>
+	<section id="about"><h2>ABOUT</h2></section>
+	<section id="portafolio"><h2>WORKS</h2></section>
+	<section id="contact"><h2>CONTACT ME</h2></section>
+	<section id="footer"></section>
+</body>
+</html>
