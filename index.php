8d326226f9ff4c177b2f5f9b388c99daf3ed553d
Fabian <fmunoz88@outlook.com>
2014-07-30 10:25:26 -0500

    se añadió el nav-bar de bootstrap responsivo

--- index.php
+++ index.php
@@ -24,53 +24,44 @@
     <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-	<nav id="navigation">
-		<div id="nav-inner">
-			<div class="logo">LOGO</div>
-			<div class="nav-menu">
-				<ul>
-					<li>
-						<a href="">MENÚ 1</a>
-					</li>
-					<li>
-						<a href="">MENÚ 2</a>
-					</li>
-					<li>
-						<a href="">MENÚ 3</a>
-					</li>
-					<li>
-						<a href="">MENÚ 4</a>
-					</li> 
-					<li class="dropdown-hover">
-						<a href="">MENÚ 5</a>
-						<ul class="dropdown-menu-1">
-							<li>
-								<a href="">opcion 1</a>
-							</li>
-							<li>
-								<a href="">opción 2</a>
-							</li>
-							<li>
-								<a href="">opcion 3</a>
-							</li>
-							<li>
-								<a href="">opcion 4</a>
-							</li>
-						</ul>
-					</li>
+	<header id="navigation" class="navbar navbar-default navbar-fixed-top background-nav" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand logo" href="#">LOGO</a>
+        </div>
+        <nav class="nav-menu navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="">MENÚ 1</a></li>
+			<li><a href="">MENÚ 2</a></li>
+			<li><a href="">MENÚ 3</a></li>
+			<li><a href="">MENÚ 4</a></li>
+			<li class="dropdown" >
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+				<ul class="dropdown-menu background-nav" role="menu">
+					<li><a href="">opcion 1</a></li>
+					<li><a href="">opción 2</a></li>
+					<li><a href="">opcion 3</a></li>
+					<li><a href="">opcion 4</a></li>
 				</ul>
-			</div>
-		</div>
-	</nav>
+            </li> 
+          </ul>
+        </div><!--/.nav-collapse -->
+      </nav>
+    </header>
 	<section id="home" class="">
 		<div id="home-inner">
 		</div>
 	</section>
 	<section id="about">
-		<div class="inner">
+		<div class="container">
 			<h2>ABOUT</h2>
 		</div>
-		
 	</section>
 	<section id="portafolio"><h2>WORKS</h2></section>
 	<section id="contact"><h2>CONTACT ME</h2></section>
