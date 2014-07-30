7bf8a3a578200793e13ed6feee46d1a570f3c4df
Fabian <fmunoz88@outlook.com>
2014-07-30 08:10:43 -0500

    Revert "Revert "Revert "merge"""
    This reverts commit 12e7abaf518ef7955573ba11e2a7805990c81973.

--- index.php
+++ index.php
@@ -4,24 +4,9 @@
 	<title>Prueba Diseño</title>
  	<meta charset="utf-8">
  	<meta name="description" content="">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/responsive.css">
-	<!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
 	<nav id="navigation">
@@ -41,22 +26,8 @@
 					<li>
 						<a href="">MENÚ 4</a>
 					</li> 
-					<li class="dropdown-hover">
+					<li>
 						<a href="">MENÚ 5</a>
-						<ul class="dropdown-menu-1">
-							<li>
-								<a href="">opcion 1</a>
-							</li>
-							<li>
-								<a href="">opción 2</a>
-							</li>
-							<li>
-								<a href="">opcion 3</a>
-							</li>
-							<li>
-								<a href="">opcion 4</a>
-							</li>
-						</ul>
 					</li>
 				</ul>
 			</div>
