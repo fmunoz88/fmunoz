29c7396419f566f6da48a6909ab70922019411bf
Fabian <fmunoz88@outlook.com>
2014-08-08 12:48:14 -0500

    se modifico el index

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 	<title>Prueba Diseño</title>
  	<meta charset="utf-8">
  	<meta name="description" content="">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/responsive.css">
