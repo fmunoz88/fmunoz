0998598fd1ea9741e3bcc9eb66c2c636306062d4
fmunoz88 <fmunoz88@outlook.com>
2015-04-07 12:24:06 -0500

    no message

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
 				<div class="inner-home">
 					<!--<h2>HOME</h2>-->
                     <svg viewBox="0 0 600 300">
-                      <text text-anchor="middle"
+                      <text id="text-header" text-anchor="middle"
                             x="50%"
                             y="50%"
                             dy=".35em"
